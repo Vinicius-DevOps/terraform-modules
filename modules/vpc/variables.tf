24ec66bd46a2c8abfc9eb81a45f299718ef56525
Vinicius Costa <costavinicius692@gmail.com>
2025-07-21 13:04:39 -0300

    feat: Upload the modules VPC

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
+  type = string
+  default = "10.0.0.0/16"
+}
+variable "environment" {
+  description = "The environment for the Resources."
+  type = string
+  default = "dev"
+}
